a2d4a08029d9f772f392b833292309d441c59a69
dahaliahowell <dahaliahowell.dh@gmail.com>
2021-04-19 20:37:33 -0500

    added frontend routes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -84,7 +84,13 @@ const NotFound = {
 // Define Routes
 const routes = [
     { path: "/", component: Home },
-    // Put other routes here
+    { path: "/register", component: Register },
+    { path: "/login", component: Login },
+    { path: "/logout", component: Logout },
+    { path: "/explore", component: Explore },
+    { path: "/users/{user_id}", component: User },
+    { path: "/cars/new", component: AddCar },
+    { path: "/cars/{card_id}", component: Car },
 
     // This is a catch all route in case none of the above matches
     { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound }
