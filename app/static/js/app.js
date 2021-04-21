a435db7698c3598a2910fc8f41f1edd0eb4245cb
dahaliahowell <dahaliahowell.dh@gmail.com>
2021-04-20 23:47:03 -0500

    Added register and login templates

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -20,10 +20,10 @@ app.component('app-header', {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <router-link class="nav-link" to="/">Register <span class="sr-only">(current)</span></router-link>
+            <router-link class="nav-link" to="/register">Register <span class="sr-only">(current)</span></router-link>
           </li>
           <li class="nav-item active">
-            <router-link class="nav-link" to="/">Login <span class="sr-only">(current)</span></router-link>
+            <router-link class="nav-link" to="/login">Login <span class="sr-only">(current)</span></router-link>
           </li>
         </ul>
       </div>
@@ -55,8 +55,8 @@ const Home = {
                 <h1 class="font-weight-bold">Buy and Sell Cars Online</h1>
                 <p class="mt-2 mb-4 text-secondary">United Auto Sales provides the fastest, easiest and most user friendly way to buy or sell cars online. Find a Great Price on the Vehicle You Want</p>
                 <div class="flex-area">
-                    <button class="btn bg-primary text-white" type="button">Resister</button>
-                    <button class="btn bg-secondary text-white" type="button">Login</button>
+                    <button @click="toRegister" class="btn bg-primary text-white" type="button">Resister</button>
+                    <button @click="toLogin" class="btn bg-secondary text-white" type="button">Login</button>
                 </div>
             </div>
             <div class="fit col-md-6">
@@ -66,7 +66,99 @@ const Home = {
     `,
     data() {
         return {}
-    }
+    }, 
+    methods: {
+        toRegister: function() {
+            this.$router.push({ path: '/register' });
+        },
+        toLogin: function() {
+            this.$router.push({ path: '/login' });
+        },
+    },
+};
+
+const Register = {
+    name: 'Register',
+    template: `
+        <div>
+            <h1>Register New User</h1>
+
+            <form method="POST" action="" id="registerForm" @submit.prevent="registerUser">
+                <div>
+                    <div>
+                        <label class="" for="username">Username</label><br>
+                        <input type="text" class="" name="username">
+                    </div>
+                    <div>
+                        <label class="" for="password">Password</label><br>
+                        <input type="password" class="" name="password">
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <label class="" for="fullname">Fullname</label><br>
+                        <input type="text" class="" name="fullname">
+                    </div>
+                    <div>
+                        <label class="" for="email">Email</label><br>
+                        <input type="email" class="" name="email">
+                    </div>
+                </div>
+                <div>
+                    <label class="" for="location">Location</label><br>
+                    <input type="text" class="" name="location">
+                </div>
+                <div>
+                    <label class="" for="biography">Biography</label><br>
+                    <textarea name="biography" class=""></textarea><br>
+                </div>
+                <div>
+                    <label class="" for="photo">Upload Photo</label><br>
+                    <input type="file" class="" name="photo" accept="image/x-png,image/jpg">
+                </div>
+
+                <button type="submit" name="submit" class="btn btn-primary">Register</button>
+            </form>
+        </div>
+    `,
+    data() {
+        return {}
+    },
+    methods: {
+        registerUser () {
+
+        }
+    },
+};
+
+const Login = {
+    name: 'Login',
+    template: `
+        <div>
+            <h1>Login to your account</h1>
+
+            <form method="POST" action="" id="loginForm" @submit.prevent="loginUser">
+                <div>
+                    <label class="" for="username">Username</label><br>
+                    <input type="text" class="" name="username">
+                </div>
+                <div>
+                    <label class="" for="biography">Password</label><br>
+                    <input type="password" class="" name="password">
+                </div>
+
+                <button type="submit" name="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    `,
+    data() {
+        return {}
+    },
+    methods: {
+        loginUser () {
+
+        }
+    },
 };
 
 const NotFound = {
@@ -84,9 +176,9 @@ const NotFound = {
 // Define Routes
 const routes = [
     { path: "/", component: Home },
-    /*{ path: "/register", component: Register },
+    { path: "/register", component: Register },
     { path: "/login", component: Login },
-    { path: "/logout", component: Logout },
+    /*{ path: "/logout", component: Logout },
     { path: "/explore", component: Explore },
     { path: "/users/:id", component: User },
     { path: "/cars/new", component: AddCar },
