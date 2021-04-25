20da82de6f920f10a833e535383e835372cf4f55
dahaliahowell <dahaliahowell.dh@gmail.com>
2021-04-25 11:32:17 -0500

    fixed profile route issue

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -28,7 +28,7 @@ app.component('app-header', {
                 <router-link class="nav-link" to="/explore">Explore <span class="sr-only">(current)</span></router-link>
             </li>
             <li class="nav-item active">
-                <router-link class="nav-link" :to="{ name: 'users', params: {id: userInfo.name}}">My Profile <span class="sr-only">(current)</span></router-link>
+                <router-link class="nav-link" :to="{ name: 'users', params: {id: userInfo.id}}">My Profile <span class="sr-only">(current)</span></router-link>
             </li>
         </ul>
       
